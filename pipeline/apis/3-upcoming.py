b390b293a2136129c54f93c72223a6401ed2a06c
kuir-juach <k.thuch@alustudent.com>
2025-03-02 21:22:06 +0200

    updated

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python3
-"""Displays the upcoming launch with these information"""
+
+""" Return the location of a specific GitHub user """
+
 import requests
-from datetime import datetime
+import sys
+import time
 
 
-if __name__ == '__main__':
-    """Displays the upcoming launch with these information"""
-    url = "https://api.spacexdata.com/v4/launches/upcoming"
-    r = requests.get(url)
-    recent = 0
+def get_user_location(api_url):
+    """
+    Fetches the location of a specific GitHub user from the provided API URL.
 
-    for dic in r.json():
-        new = int(dic["date_unix"])
-        if recent == 0 or new < recent:
-            recent = new
-            launch_name = dic["name"]
-            date = dic["date_local"]
-            rocket_number = dic["rocket"]
-            launch_number = dic["launchpad"]
+    Parameters:
+    api_url (str): The API endpoint URL to fetch user data from.
 
-    rurl = "https://api.spacexdata.com/v4/rockets/" + rocket_number
-    rocket_name = requests.get(rurl).json()["name"]
-    lurl = "https://api.spacexdata.com/v4/launchpads/" + launch_number
-    launchpad = requests.get(lurl)
-    launchpad_name = launchpad.json()["name"]
-    launchpad_local = launchpad.json()["locality"]
-    string = "{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
-                                           launchpad_name, launchpad_local)
+    Returns:
+    None: Prints the user's location or an error message if not found.
 
-    print(string)
+    Raises:
+    requests.exceptions.RequestException: If there's an HTTP request issue.
+    """
+    try:
+        res = requests.get(api_url)
+
+        if res.status_code == 403:
+            rate_limit_reset = int(res.headers.get('X-Ratelimit-Reset', 0))
+            current_time = int(time.time())
+            diff = (rate_limit_reset - current_time) // 60
+            print("Reset in {} min".format(diff))
+        elif res.status_code == 404:
+            print("Not found")
+        elif res.status_code == 200:
+            user_data = res.json()
+            location = user_data.get('location', 'Location not provided')
+            print(location)
+        else:
+            error_msg = "Error: Status code {}".format(res.status_code)
+            print(error_msg)
+
+    except requests.exceptions.RequestException as e:
+        error_msg = "An error occurred: {}".format(e)
+        print(error_msg)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <API_URL>")
+        sys.exit(1)
+
+    api_url = sys.argv[1]
+    get_user_location(api_url)
